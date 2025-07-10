03e137723181831a8421fd108fc9f1ee62060916
haneenimam <hanen.203100228@compit.aun.edu.eg>
2025-07-10 18:45:34 +0300

    Update portfolio layout and project cards

---
+++ script.js
@@ -0,0 +1,9 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const navLinks = document.querySelectorAll(".nav-link");
+
+    navLinks.forEach(link => {
+        link.addEventListener("click", function () {
+            document.querySelector(".navbar-collapse").classList.remove("show");
+        });
+    });
+});
